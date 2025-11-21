2c8937eb0ebd3ccde25c3d982635677dc13e6741
Dibyajyoti <dibyajyotipramanick0750@gmail.com>
2025-11-21 21:25:37 +0530

    changes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -20,7 +20,7 @@ export default function Navbar(): ReactElement {
   return (
     <nav className="w-full bg-white py-5 mb-5 md:mb-0 font-semibold">
       <div className="flex justify-between px-10 md:justify-around items-center">
-        <div className="flex items-center gap-2">
+        <div className="flex items-center">
           <img src={assets.logo} alt="Logo" className="w-12 h-12" />
           <p>Dibyajyoti</p>
         </div>
@@ -52,8 +52,17 @@ export default function Navbar(): ReactElement {
         </button>
       </div>
 
-      {showLinks && (
-        <div className="flex flex-col gap-2 my-4 px-10 md:hidden text-center">
+      <div
+        className={`
+        md:hidden overflow-hidden transition-all duration-300
+        ${
+          showLinks
+            ? "max-h-96 opacity-100 translate-y-0"
+            : "max-h-0 opacity-0 -translate-y-3"
+        }
+      `}
+      >
+        <div className="flex flex-col gap-2 my-4 px-10 text-center">
           {navLinks.map((item, idx) => (
             <a
               key={idx}
@@ -63,11 +72,14 @@ export default function Navbar(): ReactElement {
               {item.title}
             </a>
           ))}
-          <button className="flex items-center justify-center gap-2 bg-black text-white rounded mt-2 py-2 px-4 hover:bg-gray-900 transition cursor-pointer">
+          <button
+            onClick={() => alert("It will be available after I create one 😅")}
+            className="flex items-center justify-center gap-2 bg-black text-white rounded mt-2 py-2 px-4 hover:bg-gray-900 transition cursor-pointer"
+          >
             Resume <Download className="w-4 h-4" />
           </button>
         </div>
-      )}
+      </div>
     </nav>
   );
 }
